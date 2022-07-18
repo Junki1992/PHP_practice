5f24cb6075d3fed21311879b59cf1618f4f2ec1b
Junki1992 <junki.warner@me.com>
2022-07-18 18:46:47 +0900

    DB登録用の値を入力フォームから受け取る仕様に変更

--- sample03/index.php
+++ sample03/index.php
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sample03</title>
-</head>
-
-<body>
-    <?php
-    $db = new mysqli('localhost:8889', 'root', 'root', 'mydb_new');
-    $message = 'テスト用のコメントです02';
-    $stmt = $db->prepare('INSERT INTO training (comment) VALUES (?)');
-    if (!$stmt) {
-        die($db->error);
-    } else {
-        $stmt->bind_param('s', $message);
-        $ret = $stmt->execute();
-        echo 'テスト用のコメントを挿入しました';
-    }
-    ?>
-</body>
-
-</html>
+<?php
+$message = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_SPECIAL_CHARS);
+$db = new mysqli('localhost:8889', 'root', 'root', 'mydb_new');
+$stmt = $db->prepare('INSERT INTO training(comment) VALUES (?)');
+if (!$stmt) {
+    die($db->error);
+}
+$stmt->bind_param('s', $message);
+$success = $stmt->execute();
+if ($success) {
+    echo '登録されました。';
+} else {
+    $db->error;
+    echo '登録できませんでした';
+}
