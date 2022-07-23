b6850fe9b890ca402f241c84b553a13dd2b46974
Junki1992 <junki.warner@me.com>
2022-07-23 15:02:47 +0900

    画面からコメントを投稿する機能を実装

---
+++ mini_bbs/input_do.php
@@ -0,0 +1,11 @@
+<?php
+$comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_SPECIAL_CHARS);
+require('dbConnect.php');
+$stmt = $db->prepare('INSERT INTO training(comment) VALUES(?)');
+var_dump($stmt);
+if (!$stmt) {
+    die($db->error);
+}
+
+$stmt->bind_param('s', $comment);
+$stmt->execute();
