b1904ee0b97aa36ea0b8cd3c1631cea5926a25f7
Junki1992 <junki.warner@me.com>
2022-07-23 16:02:08 +0900

    コメントの詳細機能を実装

---
+++ mini_bbs/comment.php
@@ -0,0 +1,28 @@
+<?php
+require('dbConnect.php');
+$stmt = $db->prepare('SELECT * FROM training WHERE id=?');
+if (!$stmt) {
+    die($db->error);
+}
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$stmt->bind_param('i', $id);
+$result = $stmt->execute();
+$stmt->bind_result($id, $comment, $created);
+$result = $stmt->fetch();
+if (!$result) {
+    echo '指定された投稿は存在しません';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>コメント詳細</title>
+</head>
+<body>
+    <?php echo $comment;?>
+</body>
+</html>
