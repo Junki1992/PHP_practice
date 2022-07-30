00575e6b1ea71ef7edcc87c81028cf88199b1171
Junki1992 <junki.warner@me.com>
2022-07-30 23:08:16 +0900

    DBへの接続処理をlibrary.php内で関数化

--- register_challenge/library.php
+++ register_challenge/library.php
@@ -3,4 +3,13 @@
 function h($value) {
     return htmlspecialchars($value, ENT_QUOTES);
 }
+
+// DBへの接続
+function dbConnect() {
+    $db = new mysqli('localhost', 'root', 'root', 'bbs_challenge');
+    if (!$db) {
+        die($db->error);
+    }
+    return $db;
+}
 ?>
