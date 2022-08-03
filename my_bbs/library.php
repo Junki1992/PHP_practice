b71c58948018021387cd8f800aed730ebf6616e6
Junki1992 <junki.warner@me.com>
2022-08-03 19:33:31 +0900

    DB接続用のファンクションを作成

--- my_bbs/library.php
+++ my_bbs/library.php
@@ -3,4 +3,13 @@
 function h($values) {
     return htmlspecialchars($values, ENT_QUOTES);
 }
+
+// DB接続
+function dbConnect() {
+    $db = new mysqli('localhost', 'root', 'root', 'my_bbs');
+    if (!$db) {
+        die($db->error);
+    }
+    return $db;
+}
 ?>
